4beaf94f43aa967074eefd9a94580f202b243942
doveva <73473248+doveva@users.noreply.github.com>
2021-02-14 18:10:17 +0300

    Delete File_opener.py

--- lib/File_opener.py
+++
@@ -1,8 +0,0 @@
-import os
-
-file = '../input data/test_schedule.inc'
-if os.path.exists(file):
-    with open(file,encoding="utf-8") as f:
-        lines = f.readlines()
-        print(lines)
-        f.close()
