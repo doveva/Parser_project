b4f17e1ef9a3adfb44118031eeadab921465756a
doveva <73473248+doveva@users.noreply.github.com>
2021-03-01 22:34:06 +0300

    Тест на реальной строчке
    Вроде работает как надо (НО ЭТО НЕ ТОЧНО)

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
 from lib import parser
+import re
 if __name__ == '__main__':
     keywords = ("DATES","COMPDAT","COMPDATL")
     parameters = ""
     input_file = 'input data/test_schedule.inc'
     output_file = 'output data/schedule.csv'
 
-    schedule.df=parser.transform_schedule(keywords,parameters,input_file,output_file)
+
+print(parser.parse_keyword_COMPDATL_line("'W4' 40 30  1   3 	OPEN 	1* 	1 	2 	1 	3* 			4.0 /"))
