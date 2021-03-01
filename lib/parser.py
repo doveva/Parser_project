bb12803d6b0b8dcb2b84f7237196a54285bbcfc3
doveva <73473248+doveva@users.noreply.github.com>
2021-03-01 22:32:21 +0300

    Обновлены функции парсинга строк
    Сделано постоянное решение для Compdat 
    Дописана функция для распаковки дефолтных значений

--- lib/parser.py
+++ lib/parser.py
@@ -1,4 +1,4 @@
-import numpy
+import numpy as np
 import re
 import pandas
 import os
@@ -24,7 +24,7 @@ def read_schedule():
     return lines
 
 def parse_keyword_DATE_line(current_date_line: str):
-    char_set = set("!@#%&*()[]{}/?<>")
+    char_set = set("!@#%&()[]{}/?<>'")
     output_string = ""
     for i in range(0, len(current_date_line)):
         if not current_date_line[i] in char_set:
@@ -33,25 +33,56 @@ def parse_keyword_DATE_line(current_date_line: str):
     return parse_data
 
 def parse_keyword_COMPDAT_line(well_comp_line: str):
-    char_set = set("!@#%&*()[]{}/?<>")
+    char_set = set("!@#%&()[]{}/?<>'")
     output_string = ""
     well_comp_line = well_comp_line[:well_comp_line.find('-')]
     for i in range(0, len(well_comp_line)):
         if not well_comp_line[i] in char_set:
             output_string +=  well_comp_line[i]
+    output_string = default_params_unpacking_in_line(output_string)
+    parse_data = output_string.split()
+    parse_data.insert(1,np.nan)
+    return parse_data
+
+def parse_keyword_COMPDATL_line(well_comp_line: str):
+    char_set = set("!@#%&()[]{}/?<>'")
+    output_string = ""
+    well_comp_line = well_comp_line[:well_comp_line.find('-')]
+    for i in range(0, len(well_comp_line)):
+        if not well_comp_line[i] in char_set:
+            output_string +=  well_comp_line[i]
+    output_string = default_params_unpacking_in_line(output_string)
     parse_data = output_string.split()
     return parse_data
 
+
+def decode(match):
+    ch_st = set("*")
+    res=""
+    for i in range(0, len(str(match.group(0)))):
+        if not match.group(0)[i] in ch_st:
+            res +=  match.group(0)[i]
+    string = ""
+    for i in range(0,int(res[0])):
+        string += " DEFAULT"
+    return str(string)
+
+
+def default_params_unpacking_in_line(well_comp_line: str):
+    output_string = re.sub('([1-9]\*)', decode, well_comp_line)
+    output_string = " ".join([el for el in output_string.split(' ') if el.strip()])
+    return output_string
+
 def inspect_schedule():
-
+    return 1
 def clean_schedule():
-
+    return 1
 def parse_schedule():
-
+    return 1
 def extract_keywords_blocks():
-
+    return 1
 def extract_lines_from_keyword_block():
-
+    return 1
 def parse_keyword_block():
-
+    return 1
 
